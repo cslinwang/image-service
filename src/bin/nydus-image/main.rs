1dc1d13f9a1ba644d43dba44c277634c86ed859b
Lin Wang <l.wang@mail.dlut.edu.cn>
2023-11-06 09:54:59 +0800

    增加chunk过滤
    Signed-off-by: Lin Wang <l.wang@mail.dlut.edu.cn>

--- src/bin/nydus-image/main.rs
+++ src/bin/nydus-image/main.rs
@@ -1428,7 +1428,7 @@ impl Command {
                 image_name: String::from("redis"),
                 version_name: String::from("nydus_7.0.2"),
                 chunk_blob_id: String::from(
-                    "73483061f74471da1b80947688b92bf0fd853e8456a6d662829cf7ec88785e8d",
+                    "73483061f74471da1b80947688b92bf0fd853e8456a6d662829cf7ec88785e8a",
                 ),
                 chunk_digest: String::from(
                     "00047517875f3cd8a471ae956f654015c719f6d52220a5745da30c2eea48d33a",
@@ -1442,7 +1442,7 @@ impl Command {
                 image_name: String::from("redis"),
                 version_name: String::from("nydus_7.0.2"),
                 chunk_blob_id: String::from(
-                    "73483061f74471da1b80947688b92bf0fd853e8456a6d662829cf7ec88785e8d",
+                    "73483061f74471da1b80947688b92bf0fd853e8456a6d662829cf7ec88785e8a",
                 ),
                 chunk_digest: String::from(
                     "00096459293ffff2f057815589f662f51d85b9bfc111d34f465a31290cf6a785",
@@ -1456,7 +1456,7 @@ impl Command {
                 image_name: String::from("redis"),
                 version_name: String::from("nydus_7.0.2"),
                 chunk_blob_id: String::from(
-                    "73483061f74471da1b80947688b92bf0fd853e8456a6d662829cf7ec88785e8d",
+                    "73483061f74471da1b80947688b92bf0fd853e8456a6d662829cf7ec88785e8a",
                 ),
                 chunk_digest: String::from(
                     "003dfa491feb4729728dba4024bec506eb035d28fd9943b3ff696033cc5413b0",
