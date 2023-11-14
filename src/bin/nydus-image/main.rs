01e550a0a47eb9323c9f77ffac14a3e889b1fc63
Junduo Dong <dongjunduo.djd@antgroup.com>
2023-11-14 15:45:09 +0800

    Fix no export subcmd panic on mac
    Signed-off-by: Junduo Dong <dongjunduo.djd@antgroup.com>

--- src/bin/nydus-image/main.rs
+++ src/bin/nydus-image/main.rs
@@ -782,8 +782,6 @@ fn main() -> Result<()> {
         Command::merge(matches, &build_info)
     } else if let Some(matches) = cmd.subcommand_matches("check") {
         Command::check(matches, &build_info)
-    } else if let Some(matches) = cmd.subcommand_matches("export") {
-        Command::export(&cmd, matches, &build_info)
     } else if let Some(matches) = cmd.subcommand_matches("inspect") {
         Command::inspect(matches)
     } else if let Some(matches) = cmd.subcommand_matches("stat") {
@@ -793,8 +791,18 @@ fn main() -> Result<()> {
     } else if let Some(matches) = cmd.subcommand_matches("unpack") {
         Command::unpack(matches)
     } else {
-        println!("{}", usage);
-        Ok(())
+        #[cfg(target_os = "linux")]
+        if let Some(matches) = cmd.subcommand_matches("export") {
+            Command::export(&cmd, matches, &build_info)
+        } else {
+            println!("{}", usage);
+            Ok(())
+        }
+        #[cfg(not(target_os = "linux"))]
+        {
+            println!("{}", usage);
+            Ok(())
+        }
     }
 }
 
@@ -1762,17 +1770,6 @@ impl Command {
             path.as_ref()
         );
         Ok(())
-    }
-}
-
-#[cfg(not(target_os = "linux"))]
-impl Command {
-    fn export(
-        _args: &ArgMatches,
-        _subargs: &ArgMatches,
-        _build_info: &BuildTimeInfo,
-    ) -> Result<()> {
-        unimplemented!()
     }
 }
 
